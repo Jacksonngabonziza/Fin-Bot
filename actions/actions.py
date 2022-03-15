58aace193c5be5101d1fb6983d67c45048eb3719
Jackson <ngabojck@gmail.com>
2022-03-15 11:25:21 +0200

    rasa actions

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,35 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from typing import Any, Text, Dict, List
+#from country_list import countries_for_language
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+def getAgent(district):
+    url="https://financial-chatbot-basic.herokuapp.com/api/v1/agent/location/"
+    district=district.lower()
+    urlr=url+district
+    x=requests.get(url)
+    #print(x.text)
+    result=x.json()
+    return result
+
+
+class ActionHelloWorld(Action):
+
+     def name(self) -> Text:
+         return "action_agents"
+
+     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        district=tracker.get_slot("district")             
+        result=getAgent(district)
+        dispatcher.utter_message(
+                       f" imibare ya covid ya : are {result}."
+                   )
+        #els   
+            #dispatcher.utter_message(
+                   #    f" imibare ya covid ya {count} ni {temp}."
+                 #     
+        return
